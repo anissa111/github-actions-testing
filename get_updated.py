788137d8a7a33d5238b32fe973f23709de583b6e
anissa111 <anissaz@ucar.edu>
2024-03-29 14:10:24 -0600

    >>

--- get_updated.py
+++ get_updated.py
@@ -2,6 +2,7 @@ import json
 
 import requests
 import sys
+import os
 
 
 def get_updated():
@@ -10,12 +11,20 @@ def get_updated():
     files = response.json()
     return [file['filename'] for file in files]
 
+def find_info(context):
+    #head = context['event']['pull_request']['base']['sha']
+    base = context['event']['pull_request']['base']['ref']
+    curr = context['event']['after']
+
+    # run git diff
+    os.system(f'git diff {base}')
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
         context = json.load(f)
 
-    print(context)
+    find_info(context)
+
 
 
 
